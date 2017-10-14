5d44121852a9b2eb05cb87f5a40ef17b3a1abee0
William Swanson <swansontec@gmail.com>
2017-10-14 09:26:54 -0700

    Add commands for approving edge logins

--- src/commands/lobby.js
+++ src/commands/lobby.js
@@ -28,3 +28,49 @@ command(
     })
   }
 )
+
+command(
+  'lobby-fetch',
+  {
+    usage: 'lobbyId',
+    help: 'Fetches an edge request from the lobby server',
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 1) throw new UsageError(this)
+    const lobbyId = argv[0]
+
+    return session.account.fetchLobby(lobbyId).then(lobby => {
+      const { loginRequest } = lobby
+      console.log('loginRequest: ' + (loginRequest ? 'yes' : 'no'))
+      if (loginRequest) {
+        console.log(' appId: ' + loginRequest.appId)
+        console.log(' displayName: ' + loginRequest.displayName)
+        console.log(' displayImageUrl: ' + loginRequest.displayImageUrl)
+      }
+      return void 0
+    })
+  }
+)
+
+command(
+  'lobby-login-approve',
+  {
+    usage: 'lobbyId',
+    help: 'Approves an edge-login request',
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 1) throw new UsageError(this)
+    const lobbyId = argv[0]
+
+    return session.account.fetchLobby(lobbyId).then(lobby => {
+      const { loginRequest } = lobby
+      if (!loginRequest) {
+        throw new Error('This lobby is not requesting an edge login.')
+      }
+
+      return loginRequest.approve()
+    })
+  }
+)
