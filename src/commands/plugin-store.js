5bd85de3d19bc4ebd0d15fd36e413adf6d5128b8
William Swanson <swansontec@gmail.com>
2018-06-04 15:23:55 -0700

    Add plugin-data commands

---
+++ src/commands/plugin-store.js
@@ -0,0 +1,79 @@
+import { UsageError, command } from '../command.js'
+
+command(
+  'plugin-data-list',
+  {
+    usage: '[<plugin-id>]',
+    help: 'Lists the either plugins in the store, or the items within a plugin',
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 0 && argv.length !== 1) throw new UsageError(this)
+    const pluginId = argv[0]
+
+    if (pluginId) {
+      return session.account.pluginStorage
+        .listItems(pluginId)
+        .then(names => console.log(names))
+    } else {
+      return session.account.pluginStorage
+        .listPlugins()
+        .then(names => console.log(names))
+    }
+  }
+)
+
+command(
+  'plugin-data-delete',
+  {
+    usage: '<plugin-id> [<item-name>]',
+    help: "Deletes the either a single item, or an entire plugin's data",
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 1 && argv.length !== 2) throw new UsageError(this)
+    const pluginId = argv[0]
+    const itemId = argv[1]
+
+    if (itemId) {
+      return session.account.pluginStorage.deleteItem(pluginId, itemId)
+    } else {
+      return session.account.pluginStorage.clearPlugin(pluginId)
+    }
+  }
+)
+
+command(
+  'plugin-data-get',
+  {
+    usage: '<plugin-id> <item-name>',
+    help: 'Gets an item from a plugin data store',
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 2) throw new UsageError(this)
+    const pluginId = argv[0]
+    const itemId = argv[1]
+
+    return session.account.pluginStorage
+      .getItem(pluginId, itemId)
+      .then(text => console.log(text))
+  }
+)
+
+command(
+  'plugin-data-set',
+  {
+    usage: '<plugin-id> <item-name> <text>',
+    help: 'Puts an into a plugin data store',
+    needsAccount: true
+  },
+  function (console, session, argv) {
+    if (argv.length !== 3) throw new UsageError(this)
+    const pluginId = argv[0]
+    const itemId = argv[1]
+    const text = argv[2]
+
+    return session.account.pluginStorage.setItem(pluginId, itemId, text)
+  }
+)
